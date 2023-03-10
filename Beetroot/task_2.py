5fc42a9000505745e3f1e752c2b2840fa8f8aace
StaniSlavuS <stanislav.zelinskyy@gmail.com>
2023-03-10 10:09:26 +0100

    Create a python program named "task2"

---
+++ Beetroot/task_2.py
@@ -0,0 +1,22 @@
+# Hello Word
+print("Hello Word")
+
+# Hello 'name'
+# How are you?
+a = "Hello "
+b = "\nHow are you?"
+YourName = input("enter your Name hier:")
+print(a + YourName + b)
+
+#Beetroot-->Academy-->online-->courses
+print("Beetroot", "Academy", "online", "courses", sep="-->")
+
+#Beetroot
+#*****
+#Academy
+#*****
+#Online
+#*****
+#Courses
+#*****
+print("Beetroot", "Academy", "Online", "Courses", sep="\n*****\n", end="\n*****")
