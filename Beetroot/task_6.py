dd6deddc8cec751324020874960754438ae42005
StaniSlavuS <stanislav.zelinskyy@gmail.com>
2023-03-10 13:01:24 +0100

    The greeting program for task 3 homework 11 Mar.

---
+++ Beetroot/task_6.py
@@ -0,0 +1,16 @@
+a = int(input("Enter an integer 'a': "))
+b = int(input("Enter an integer 'b': "))
+addition_ab = a + b
+subtraction_ab = a - b
+division_ab = a / b
+multiplication_ab = a * b
+exponent_ab = a ** b
+modulus_ab = a % b
+floor_division_ab = a // b
+print(addition_ab, 'Addition', end ='\n')
+print(subtraction_ab, 'Subtraction', end='\n')
+print(division_ab, 'Division', end='\n')
+print(multiplication_ab, 'Multiplication', end='\n')
+print(exponent_ab, 'Exponent', end='\n')
+print(modulus_ab, 'Modulus', end='\n')
+print(floor_division_ab, 'Floor division')
