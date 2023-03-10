5adcaf29fc8da7ae2c30f1f80a0e1f8646a19095
StaniSlavuS <stanislav.zelinskyy@gmail.com>
2023-03-10 12:20:36 +0100

    The greeting program for task 1 homework 11 Mar.

---
+++ Beetroot/task_4.py
@@ -0,0 +1,6 @@
+import datetime
+my_name = input("Enter your name: ")
+date_time = datetime.datetime.now()
+greeting_text = "Good day "
+text = " is a perfect day to learn some python."
+print(greeting_text, my_name, "!", date_time, text)
