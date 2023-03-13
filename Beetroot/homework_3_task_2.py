05d8200e49c36c25b381c7a21162fed48c14e0ca
StaniSlavuS <stanislav.zelinskyy@gmail.com>
2023-03-13 10:54:11 +0100

    Homework_3 Variables and primitive data types

---
+++ Beetroot/homework_3_task_2.py
@@ -0,0 +1,8 @@
+# first_name = input("Enter your First Name: ")
+# last_name = input("Enter your Last Name: ")
+# greeting_text = "God day"
+# print(greeting_text, first_name, last_name, sep= " ", end="!")
+my_firstname = 'Stanislav'
+my_lastname = 'Zelinskyi'
+Greetenig_text = 'Good day '
+print(Greetenig_text + my_firstname + ' ' + my_lastname + '!')
--- Beetroot/task_5.py
+++
@@ -1,4 +0,0 @@
-first_name = input("Enter your First Name: ")
-last_name = input("Enter your Last Name: ")
-greeting_text = "God day"
-print(greeting_text, first_name, last_name, sep= " ", end="!")
