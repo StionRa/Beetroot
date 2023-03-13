c70b7609b9781d2f68d2ff25cb9e4be38c6f27fe
StaniSlavuS <stanislav.zelinskyy@gmail.com>
2023-03-13 16:21:57 +0100

    Homework_5 Task 1 and 2

---
+++ Beetroot/homework_5_task_2.py
@@ -0,0 +1,6 @@
+first_name = str(input("Your First Name: ")) #Input user First name
+last_name = str(input(" Your last Name: ")) #Input user Last name
+your_age = int(input("Your Age: ")) # Input user Age
+greetings = "Good day" # Greetings
+#Print greeting for user whit age calculate
+print(greetings, first_name, last_name, "on your next birthday you’ll be", your_age + 1, "years old")
--- Beetroot/main.py
+++ Beetroot/main.py
@@ -1,23 +1,45 @@
-# day_name=int(input(" Введите день недели: "))
+# # day_name=int(input(" Введите день недели: "))
+# #
+# # if day_name == 1:
+# #     print('Monday')
+# # elif day_name == 2:
+# #     print('Tuesday')
+# # elif day_name == 3:
+# #     print('Wensday')
+# # elif day_name == 4:
+# #     print('Thusday')
+# # elif day_name == 5:
+# #     print(' Friday')
+# # elif day_name == 6:
+# #     print('Suturday')
+# # elif day_name ==7:
+# #     print('Sunday')
+# # else:
+# #     print('Error')
+# #
+# # print(day_name)
 #
-# if day_name == 1:
-#     print('Monday')
-# elif day_name == 2:
-#     print('Tuesday')
-# elif day_name == 3:
-#     print('Wensday')
-# elif day_name == 4:
-#     print('Thusday')
-# elif day_name == 5:
-#     print(' Friday')
-# elif day_name == 6:
-#     print('Suturday')
-# elif day_name ==7:
-#     print('Sunday')
-# else:
-#     print('Error')
 #
-# print(day_name)
+# print(__import__("calendar").day_name[int(input())])
 
+import random
 
-print(__import__("calendar").day_name[int(input())])
+user_name: str = input("Please insert name: ")
+print("Greeting, " + str(user_name))
+print("Let`s play rock, papper, scissors")
+player = input("Choise rock, papper or scrissors by typing r, p or s: ")
+if player == 'r' or player == 'p' or player == 's':
+    computer = random.randint(1, 3)
+    # 1 == r
+    # 2 == p
+    # 3 == s
+    if (computer == 1 and player == 'r' or computer == 2 and player == 'p' or
+        computer == 3 and player == "s"):
+        print("It is draw")
+    elif (computer == 1 and player == "p" or computer == 2 and player == "s" or
+        computer == 3 and player == "r"):
+        print("Congratulations, You Win!!!")
+    else:
+        print("You Lost!")
+else:
+    print("Your input was wrong format, no game for you")
