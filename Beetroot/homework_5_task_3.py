bce93e4100c6b5a6ddff5b41de32dbc5fd2f5c61
StaniSlavuS <stanislav.zelinskyy@gmail.com>
2023-03-13 22:19:25 +0100

    Homework_5 Task 1 and 2 and 3

--- Beetroot/homework_5_task_3.py
+++ Beetroot/homework_5_task_3.py
@@ -1,8 +1,14 @@
-import random # Import random module
-test_list = str(input("Text: ")) # input text from user
-i = 0
-while i < 5: #generating while loops
-    print(''.join(random.choice(test_list) for i in range(5))) # generating new word from user word in range 5 digits
-    if i == 5: # cycle step check
-        break
-    i += 1
+# import random # Import random module
+# test_list = str(input("Text: ")) # input text from user
+# i = 0
+# while i < 5: #generating while loops
+#     print(''.join(random.choice(test_list) for i in range(5))) # generating new word from user word in range 5 digits
+#     if i == 5: # cycle step check
+#         break
+#     i += 1
+
+import random
+enter_text = input("Your text: ")
+enter_text = list(enter_text)
+random.shuffle(enter_text)
+print(''.join(enter_text))
