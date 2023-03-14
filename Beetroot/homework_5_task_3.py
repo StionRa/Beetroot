973ae588b32b2b8efdafc51b98e4b39f63442e75
StaniSlavuS <stanislav.zelinskyy@gmail.com>
2023-03-14 10:13:16 +0100

    Homework_5 Task 1 and 2 and 3 modified the program in homework 5 task 3

--- Beetroot/homework_5_task_3.py
+++ Beetroot/homework_5_task_3.py
@@ -1,14 +1,10 @@
-# import random # Import random module
-# test_list = str(input("Text: ")) # input text from user
-# i = 0
-# while i < 5: #generating while loops
-#     print(''.join(random.choice(test_list) for i in range(5))) # generating new word from user word in range 5 digits
-#     if i == 5: # cycle step check
-#         break
-#     i += 1
-
-import random
-enter_text = input("Your text: ")
-enter_text = list(enter_text)
-random.shuffle(enter_text)
-print(''.join(enter_text))
+import random # import random module
+enter_text = input("Enter your text, number or value: ") # asking the user to enter text or digits to generate
+enter_text = list(enter_text) # translate the text entered by the user into a list
+i = 0 # specify the initial step for the cycle while
+while i < 5: # we run the cycle while indicating to check which step it is, if it is less than 5, then we execute the cycle
+    random.shuffle(enter_text) # we generate unique letter combinations from the text entered by the user
+    print(''.join(enter_text)) # output using the print function the resulting word once per line without spaces
+    if i == 5: # check the loop, if the loop step is 5, perform a break on the next line
+        break
+    i += 1 # if the loop step is less than 5, add +1 to "i" and return to the beginning of the loop
