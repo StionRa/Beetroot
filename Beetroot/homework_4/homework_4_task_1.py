046a660fcdfeed0d2e7b4270fc2f10c925592aec
StaniSlavuS <stanislav.zelinskyy@gmail.com>
2023-03-15 00:31:17 +0100

    Убрал лишний str перед input добавил возможность вывода одного символа прогама работает

--- Beetroot/homework_4/homework_4_task_1.py
+++ Beetroot/homework_4/homework_4_task_1.py
@@ -1,5 +1,5 @@
 # Taking input from the user
-enter_text = str(input('Enter your text hier: '))
+enter_text = input('Enter your text hier: ')
 
 # Creating formatted string
 newString = "{}{}".format(enter_text[0:2], enter_text[-2:])
@@ -10,4 +10,4 @@ if l >= 2:
     print("Input string: " + enter_text + '\n' + "New String: " + newString)
 else:
     # print("Input string: " + enter_text + '\n' + "Empty String")
-    print("")
+    print(enter_text)
