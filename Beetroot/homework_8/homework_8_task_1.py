a9b2bded9990aa2ebf80f003effb994ed3fa476e
StaniSlavuS <stanislav.zelinskyy@gmail.com>
2023-03-22 15:26:01 +0100

    Homework 8 Task 1  A simple function.Рекомендації: Задача 1. В Python, як правило, функції пишуться вверху, код - знизу. Не змішуються. Task 2 Creating a dictionary. Task 3 A simple calculator.

--- Beetroot/homework_8/homework_8_task_1.py
+++ Beetroot/homework_8/homework_8_task_1.py
@@ -1,6 +1,6 @@
-title = input("Enter your favorite movie: ")
-def favorite_movie():
 
+def favorite_movie():
+    title = input("Enter your favorite movie: ")
     print("My favorite movie is named " + title)
 
 favorite_movie()
