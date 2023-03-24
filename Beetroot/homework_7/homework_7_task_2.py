07bc2a700fa77cf36b894db00c52df7acb26eb37
StaniSlavuS <stanislav.zelinskyy@gmail.com>
2023-03-24 17:57:59 +0100

    Задача 2. (не зарах)
    Не виконана умова "Compute the total price of the stock...", а твоя програма вираховує лише ціну однієї позиції.
    
    Крім того, твій for завжди йде по правому елементу (stock and prices), тобто по prices.
    І якщо  список prices буде довший за stock, то ця конструкція total_price[key] = stock[key] поверне виключення, тобто програма закрешиться, тому що йде звертання до неіснуючого індексу.
    
    # если ключа еще нет - создаем
    
    except KeyError
    
        total_price[key] = stock[key]
    
    Задача 4.
    А для чого цей код в програмі ?
    
    day_list = [1, 'Monday', 2, 'Tuesday', 3, 'Wednesday', 4, 'Thursday', 5, 'Friday', 6, 'Saturday', 7, 'Sunday']
    day_dict = {1: 'Monday', 2: 'Tuesday', 3: 'Wednesday', 4:'Thursday', 5: 'Friday', 6: 'Saturday', 7: 'Sunday'}
    
    Взагалі то краще було б вирішити через генератор списків, або цикл, але нехай буде zip (просто з ним прийшлось ще вводити day = [1, 2, 3, 4, 5, 6, 7], а можна було без цього )
    
    Fixed tasks 2 and 4, but with 4 I could not make it even easier
    day_a = ['Monday', 'Tuesday', 'Wednesday',' Thursday',' Friday', 'Saturday', 'Sunday']
    day_m = [i for i in range(1, 8)]
    day_x = {day_a[i]: day_m[i] for i in range(len(day_a))}
    day_rev = {day_m[i]: day_a[i] for i in range(len(day_a))}
    how to shorten and link this code

--- Beetroot/homework_7/homework_7_task_2.py
+++ Beetroot/homework_7/homework_7_task_2.py
@@ -1,14 +1,4 @@
 stock = {"banana": 6, "apple": 0, "orange": 32, "pear": 15}
-prices = {"banana": 4, "apple": 2, "orange": 1.5, "pear": 3}
-#  результирующий словарь
-total_price = {}
-# пробегаем по ключам словаря
-for key in stock and prices:
-    try:
-        # умножаем значения
-        total_price[key] = stock[key] * prices[key]
-    # если ключа еще нет - создаем
-    except KeyError:
-        total_price[key] = stock[key]
-# Выводим результат
-print(total_price)
+prices = {"banana": 4, "apple": 2, "orange": 1.5, "pear": 3, "app": 15}
+merged_dict = {key: stock.get(key, 0) * prices.get(key, 0) for key in set(stock) | set(prices)}
+print(merged_dict)
--- Beetroot/homework_7/homework_7_task_4.py
+++ Beetroot/homework_7/homework_7_task_4.py
@@ -1,9 +1,15 @@
+# day_a = ['Monday', 'Tuesday', 'Wednesday',' Thursday',' Friday', 'Saturday', 'Sunday']
+# day = [1, 2, 3, 4, 5, 6, 7]
+# x = dict(zip(day_a, day))
+# y = dict(zip(day, day_a))
+# print(day_a)
+# print(x)
+# print(y)
 day_a = ['Monday', 'Tuesday', 'Wednesday',' Thursday',' Friday', 'Saturday', 'Sunday']
+day_m = [i for i in range(1, 8)]
+day_x = {day_a[i]: day_m[i] for i in range(len(day_a))}
+day_rev = {day_m[i]: day_a[i] for i in range(len(day_a))}
 print(day_a)
-day = [1, 2, 3, 4, 5, 6, 7]
-day_list = [1, 'Monday', 2, 'Tuesday', 3, 'Wednesday', 4, 'Thursday', 5, 'Friday', 6, 'Saturday', 7, 'Sunday']
-day_dict = {1: 'Monday', 2: 'Tuesday', 3: 'Wednesday', 4:'Thursday', 5: 'Friday', 6: 'Saturday', 7: 'Sunday'}
-x = dict(zip(day_a, day))
-print(x)
-y = dict(zip(day, day_a))
-print(y)
+print(day_m)
+print(day_x)
+print(day_rev)
