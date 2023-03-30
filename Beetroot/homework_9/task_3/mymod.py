bcb36adf579300b7c7446bf669cdb217cd63e37b
StionRa <stanislav.zelinskyy@gmail.com>
2023-03-30 19:18:18 +0200

    В задачі 3 (не зарах.) ти з функції test() не робиш виклик двох інших функцій, як це передбачено умовою задачі.
    made all the necessary corrections, according to the correction of the teacher.

--- Beetroot/homework_9/task_3/mymod.py
+++ Beetroot/homework_9/task_3/mymod.py
@@ -12,11 +12,16 @@ def count_chars(name):
 print(count_chars('text.txt'))
 
 
+# def test(name):
+#     with open(name) as f:
+#         line_1 = len(f.read())
+#         f.seek(0)
+#         line_2 = len(f.readlines())
+#         f.close()
+#     return line_2, line_1
+# print(test('text.txt'))
+
 def test(name):
-    with open(name) as f:
-        line_1 = len(f.read())
-        f.seek(0)
-        line_2 = len(f.readlines())
-        f.close()
-    return line_2, line_1
+    return count_lines(name), count_chars(name)
+
 print(test('text.txt'))
